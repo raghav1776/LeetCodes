540994b9b5f0ba028eb4f25643c4a12f085c6c37
Raghvendra321 <107914562+Raghvendra321@users.noreply.github.com>
2024-01-18 15:45:14 +0530

    Palindrom

---
+++ src/main/java/Palindrome.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+public class Palindrome {
+
+    public boolean isPalindrome(int x) {
+        String a = String.valueOf(x);
+
+        StringBuilder sb = new StringBuilder(a);
+
+        String b = sb.reverse().toString();
+
+        for (int i = 0; i <a.length() ; i++) {
+            if(a.charAt(i) != b.charAt(i)){
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
